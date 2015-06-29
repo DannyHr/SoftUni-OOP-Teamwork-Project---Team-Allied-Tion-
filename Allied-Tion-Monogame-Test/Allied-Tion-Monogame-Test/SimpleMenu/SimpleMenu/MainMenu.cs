2692d579fb6a0bd1cf17dd1acd44dcebaefbd667
Stanislav Stanev <donstany79@gmail.com>
2015-06-29 19:48:41 +0300

    added menu

---
+++ Allied-Tion-Monogame-Test/Allied-Tion-Monogame-Test/SimpleMenu/SimpleMenu/MainMenu.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SimpleMenu
+{
+    public class MainMenu //contains our all gui elements stany
+    {
+        List<GUIElement> main = new List<GUIElement>();
+
+        public MainMenu() // parametreless ctor
+        {
+            main.Add(new GUIElement("content_game/grunge-texture5")); // menu
+            main.Add(new GUIElement("content_game/PlayBtn"));
+        }
+
+        public void LoadContent(ContentManager content) // validation
+        {
+            foreach (GUIElement element in main)
+            {
+                element.LoadContent(content);
+                element.CenterElement(800, 600);
+                element.clickEvent +=  this.OnClick;
+
+            }
+           // main.Find(x => x.AssetName == "PlayBtn").MoveElement(0,-10);
+
+        }
+
+        public void Update()
+        {
+            foreach (GUIElement element in main)
+            {
+                element.Update();
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            foreach (GUIElement element in main)
+            {
+                element.Draw(spriteBatch);
+            }
+        }
+
+        public void OnClick(string element)
+        {
+            if (element == "PlayBtn")
+            {
+                // Play the game
+            }
+            else if (element == "AboutBtn")
+            {
+                // About 
+            }
+        
+        }
+    }
+}
